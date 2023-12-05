26e71f69603987862e074b0adb1e5d2f6f821115
Joseggi <lks004@naver.com>
2023-12-05 18:13:32 +0900

    luxon 설치 및 사용

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,31 +2,22 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-
+import MainClock from "../components/MainClock";
 
 const HomeLayout = styled.div`
   width: 100%;
   height: 100vh;
-  background: linear-gradient( 110deg, #012840 20%, #F29C6B, #F2766B);
+  background: linear-gradient( 110deg, #FFBDC3, #83B3CC);
 `
 
 
 export default function Home() {
   let [backgroundTemp, setbgTemp] = useState('#ADC8D9');
-  let [userData, setUserData] = useState();
-
-  useEffect(()=>{
-    axios.get('https://geolocation-db.com/json/')
-    .then((res) => [
-      setUserData(res.data),
-      console.log(userData)
-    ])
-  },[])
+  
 
   return (
     <HomeLayout color={backgroundTemp}>
-      <div>현재시간 : {new Date().toLocaleTimeString()}</div>
-      <div>ip : {userData.city}</div>
+      <MainClock></MainClock>
     </HomeLayout>
   )
 }
